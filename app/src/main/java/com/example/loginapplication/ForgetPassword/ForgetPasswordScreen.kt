b3b5e2d545a4a702fa19e9b2dbc06e3164b3901f
Guto <guto050506@gmail.com>
2026-02-27 13:24:37 -0300

    feat : add ForgetPasswordScreen

--- app/src/main/java/com/example/loginapplication/ForgetPassword/ForgetPasswordScreen.kt
+++ app/src/main/java/com/example/loginapplication/ForgetPassword/ForgetPasswordScreen.kt
@@ -1,11 +1,145 @@
 package com.example.loginapplication.ForgetPassword
 
+import android.graphics.Color
+import android.graphics.drawable.shapes.Shape
+import android.hardware.lights.Light
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginapplication.ui.theme.Black
+import com.example.loginapplication.ui.theme.Blue
+import com.example.loginapplication.ui.theme.Gray
+import com.example.loginapplication.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ForgetPassword(){
+    var email by remember { mutableStateOf(value = "") }
 
+    Column(
+        modifier = Modifier
+            .background(color = White)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        TopAppBar(
+            modifier = Modifier.fillMaxWidth()
+                .size(200.dp),
+
+            title = {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(10.dp)
+                ) {
+                    Button(
+                        onClick = {},
+
+                        modifier = Modifier
+                            .size(90.dp, 70.dp)
+                            .padding(10.dp),
+
+                        colors = ButtonDefaults.buttonColors(containerColor = Blue)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Voltar tela",
+
+                            modifier = Modifier.size(20.dp),
+                            tint = Black
+
+                        )
+                    }
+
+                    Text(
+                        text = "Forget Password",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+
+                        modifier = Modifier.padding(10.dp)
+                    )
+                }
+            },
+
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = White)
+        )
+
+        
+
+
+        Spacer(
+            modifier = Modifier.width(100.dp)
+        )
+
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+
+                label = {
+                    Text(
+                        text = "Email"
+                    )
+                }
+            )
+
+            Button(
+                onClick = {
+
+                },
+
+                colors = ButtonDefaults.buttonColors(containerColor = Blue),
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(35.dp)
+            ) {
+                Text(
+                    text = "Send Email",
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+    }
 }
 
 
